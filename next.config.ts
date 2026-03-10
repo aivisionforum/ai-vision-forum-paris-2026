954ce727841fcc77383801ff7c177466697f43f7
Edward Tan <guofoo@gmail.com>
2026-03-10 14:33:42 -0700

    Remove basePath and assetPrefix from config
    Removed basePath and assetPrefix for the project.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/ai-vision-forum-paris-2026',
-  assetPrefix: '/ai-vision-forum-paris-2026/',
   images: {
     unoptimized: true, // Required for static export
   },
